169346c2136eeae444d38b038cd5323492b66712
AliIdrees1011 <aliidrees1011@gmail.com>
2025-08-28 04:44:39 +0500

    db.php

--- db.php
+++ db.php
@@ -1,12 +1,11 @@
 <?php
-$DB_HOST = 'localhost';
-$DB_NAME = 'u635821533_LbhOO';
-$DB_USER = 'u635821533_ykUFD';
-$DB_PASS = '2ADCO51#;k';
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+$conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
-  die('DB connection error: ' . $conn->connect_error);
+    die('Connection failed: ' . $conn->connect_error);
 }
-$conn->set_charset('utf8mb4');
 ?>
